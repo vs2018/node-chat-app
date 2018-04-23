30ab6dc44b50948ffdd641e7127080e83716b740
Vishal Sharma <vsh1819@gmail.com>
2018-04-23 17:18:47 +0100

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,21 @@ socket.on('disconnect', function () {
   console.log('User was disconnected');
 })
 
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault()
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function() {
+
+  })
+})
+
+
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
+  let li = jQuery('<li></li>')
+  li.text(`${message.from}: ${message.text}`)
+  jQuery('#messages').append(li)
 })
--- server/server.js
+++ server/server.js
@@ -15,13 +15,16 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
   console.log('new user connected');
 
-  socket.emit('newMessage', generateMessage('Admin', 'New user joined'))
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'))
 
-  socket.on('createMessage', (message) => {
+  socket.emit('newMessage', generateMessage('Admin', 'New user joined'))
+
+
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text))
+    callback('this is from the server')
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
     //   text: message.text,
