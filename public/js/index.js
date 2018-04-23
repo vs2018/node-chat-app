e408a34f4efacc35ac98e9853ea47960c85bb08b
Vishal Sharma <vsh1819@gmail.com>
2018-04-23 15:55:49 +0100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ let socket = io()
 
 socket.on('connect', function () {
   console.log('connected')
-
-  socket.emit('createMessage', {
-    from: 'Andrew',
-    text: 'hey there john!'
-  })
 })
 
 socket.on('disconnect', function () {
